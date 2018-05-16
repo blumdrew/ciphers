cc451e3e1662fb14d6a49a56adc0e9e47abff17d
Andrew Lindstrom <38860090+blumdrew@users.noreply.github.com>
2018-05-16 11:55:08 -0400

    Create Vigenere.py

---
+++ Vigenere.py
@@ -0,0 +1,92 @@
+#two functions, one to encrypt using a Vigenère cipher, the other to decrypt.
+#only supports lowercase letters and numbers at the moment, will change that soon
+#https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher to learn more about this sort of cipher
+
+def vig_cipher(message, keyword):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    #remove spaces and special chars
+    new_message = ''.join(filter(str.isalnum, message))
+    #only want lowercase letters
+    message = new_message.lower()
+    row = [index for index in range(len(alphabet))]
+    #table makes a square of rows, each one shifted by n places
+    table = [row[x:] + row[:x] for x in range(len(alphabet))]
+    
+    #to encode, need full key
+    if len(keyword) > len(message):
+        #if key is longer than message, set key to first n letters of keyword
+        full_key = keyword[:len(message)]
+    elif len(keyword) == len(message):
+        full_key = keyword
+    else:
+        full_fits = len(message) // len(keyword)
+        remainder = len(message) % len(keyword)
+        full_key = ''
+        for index in range(full_fits + 1):
+            full_key += keyword
+        #add on extra
+        full_key += keyword[:remainder]
+
+    #Now, ready to encrypt message
+    encrypted_message = ''
+    current_index = 0
+    for letter in message:
+        letter_index = alphabet.find(letter)
+        key_index = alphabet.find(full_key[current_index])
+        #encrypted value for char n is at point (message_n,key_n)
+        encrypted_letter = alphabet[table[letter_index][key_index]]
+        encrypted_message += encrypted_letter
+        current_index += 1
+
+    return encrypted_message
+
+def decrypt_vig(message, keyword):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    #remove spaces and special chars
+    new_message = ''.join(filter(str.isalnum, message))
+    #only want lowercase letters
+    message = new_message.lower()
+    row = [index for index in range(len(alphabet))]
+    table = [row[x:] + row[:x] for x in range(len(alphabet))]
+
+    #need full key to decode
+    if len(keyword) > len(message):
+        #if key is longer than message, set key to first n letters of keyword
+        full_key = keyword[:len(message)]
+    elif len(keyword) == len(message):
+        full_key = keyword
+    else:
+        full_fits = len(message) // len(keyword)
+        remainder = len(message) % len(keyword)
+        full_key = ''
+        for index in range(full_fits + 1):
+            full_key += keyword
+        #add on extra
+        full_key += keyword[:remainder]
+
+    decrypt_message = ''
+    current_index = 0
+    for letter in message:
+        key_row_index = alphabet.find(full_key[current_index])
+        key_row = table[key_row_index]
+        letter_index = alphabet.find(letter)
+        letter = ''
+        for index in range(len(key_row)):
+            if key_row[index] == letter_index:
+                letter = alphabet[index]
+                break
+        decrypt_message += letter
+        current_index += 1
+    return decrypt_message
+    
+    
+ def main():
+    #quick main function to encode a message
+    message = input('Enter the message you wish to encode: ')
+    key = input('Enter your keyword: ')
+    encoded = vig_cipher(message, key)
+    print('Your encoded message is: ' + encoded)
+    return
+
+if __name__ == '__main__':
+    main()
